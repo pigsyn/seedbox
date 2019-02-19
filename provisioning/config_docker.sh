fe59ef0191e9c17750ca1c2c729adcb240bdcda5
pigsyn <soulard.morgan@gmail.com>
2019-02-19 20:24:34 +0100

    Update config_docker.sh

--- provisioning/config_docker.sh
+++ provisioning/config_docker.sh
@@ -4,3 +4,4 @@ torrent_uid=$(grep torrent /etc/passwd|cut -d ':' -f 3)
 sed -i "s/1001/${torrent_uid}/g" /root/docker-compose.yml
 systemctl enable docker
 service docker start
+docker-compose -f /root/docker-compose.yml up -d
